a04a05b127e115d85f01754b9e20b5dad7b68c0d
ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>
2023-12-04 13:35:32 +0300

    Just testing remains

--- main.js
+++ main.js
@@ -34,13 +34,18 @@ class LinkedList {
     this.HEAD = node;
   }
 
-  print() {
+  toString() {
     let head = this.HEAD;
-
+    let index = 0;
+    let srt = "";
     while (head != null) {
-      console.log(head);
+      //console.log(head);
+      srt += index == 0 ? `(${head.value}) -> ` : ` (${head.value}) -> `;
       head = head.nextNode;
+      index++;
     }
+    srt += "null";
+    console.log(srt);
   }
 
   forEach(callBack) {
@@ -48,12 +53,68 @@ class LinkedList {
     let index = 0;
 
     while (head != null) {
-      console.log(head);
+      // console.log(head);
       callBack(head, index);
 
       head = head.nextNode;
       index++;
     }
+  }
+  size() {
+    let count = 0;
+    this.forEach((val, index) => (count = index));
+    console.log(count + 1);
+  }
+  head() {
+    return this.HEAD;
+  }
+  tail() {
+    return this.TAIL;
+  }
+  at(index) {
+    let head = this.HEAD;
+    let i = 0;
+    if (index < 0) {
+      return null;
+    }
+    while (head != null) {
+      if (i === index) {
+        return head;
+      }
+      head = head.nextNode;
+      i++;
+    }
+    return null;
+  }
+  contains(value) {
+    let head = this.HEAD;
+    let index = 0;
+    while (head != null) {
+      if (head.value === value) {
+        return true;
+      }
+      head = head.nextNode;
+      index++;
+    }
+    return false;
+  }
+  find(value) {
+    let head = this.HEAD;
+    let index = 0;
+    while (head != null) {
+      if (head.value === value) {
+        return index;
+      }
+      head = head.nextNode;
+      index++;
+    }
+    return null;
+  }
+  pop() {
+    const head = this.HEAD;
+    this.HEAD = this.HEAD.nextNode;
+
+    return head;
   }
 }
 
@@ -66,11 +127,16 @@ const n0 = Node("value0");
 // n1.value = "value2";
 // console.log(n1);
 const l1 = new LinkedList();
-// l1.append(n1);
-// l1.append(n2);
-// l1.append(n3);
-// l1.append(n4);
+l1.append(n1);
+l1.append(n2);
+l1.append(n3);
+l1.append(n4);
 l1.prepend(n0);
 // l1.print();
-l1.forEach((node) => console.log(node.value));
+// l1.forEach((node) => console.log(node.value));
 // console.table(l1);
+l1.size();
+console.log(l1.head());
+console.log(l1.tail());
+console.log(`at ${l1.at(2)} not at ${l1.at(9)}`);
+console.log(`true: ${l1.contains('value1')} false: ${l1.contains('here')}`)
